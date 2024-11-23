a8203441463312f93533d245876acca5bda7a74b
Andjelina Kumanova <angelinakumanova@gmail.com>
2024-11-23 14:24:51 +0200

    initialized project

---
+++ HospitalManagementApp/src/main/java/bg/hospital/hospitalmanagementapp/HospitalManagementAppApplication.java
@@ -0,0 +1,13 @@
+package bg.hospital.hospitalmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementAppApplication.class, args);
+    }
+
+}
