0d6021437f9878184e170f7f2eec684de5df4bc8
Andjelina Kumanova <angelinakumanova@gmail.com>
2024-11-28 01:35:45 +0200

    added home button

--- DoctorlyApp/src/main/java/bg/doctorly/doctorlyapp/web/controllers/SearchController.java
+++ DoctorlyApp/src/main/java/bg/doctorly/doctorlyapp/web/controllers/SearchController.java
@@ -1,7 +1,9 @@
 package bg.doctorly.doctorlyapp.web.controllers;
 
 import bg.doctorly.doctorlyapp.data.entites.Doctor;
+import bg.doctorly.doctorlyapp.service.CityService;
 import bg.doctorly.doctorlyapp.service.DoctorService;
+import bg.doctorly.doctorlyapp.service.SpecializationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +17,13 @@ import java.util.List;
 public class SearchController {
 
     private final DoctorService doctorService;
+    private final SpecializationService specializationService;
+    private final CityService cityService;
 
-    public SearchController(DoctorService doctorService) {
+    public SearchController(DoctorService doctorService, SpecializationService specializationService, CityService cityService) {
         this.doctorService = doctorService;
+        this.specializationService = specializationService;
+        this.cityService = cityService;
     }
 
 
@@ -25,9 +31,14 @@ public class SearchController {
     public String search(@RequestParam(required = false) String specialization,
                          @RequestParam(required = false) String city,
                          Model model) {
+        model.addAttribute("specializations", specializationService.getAll());
+        model.addAttribute("cities", cityService.getAll());
+        model.addAttribute("currentSpec", specialization);
+        model.addAttribute("currentCity", city);
         List<Doctor> doctors = doctorService.searchDoctors(specialization, city);
 
         model.addAttribute("doctors", doctors);
+
         if (doctors.isEmpty()) {
             model.addAttribute("message", "No doctors available for specific criteria");
         }
